5126ac275fa8ebbd7ee278b0cd48eaca66023009
oksbwn <oksbwn@gmail.com>
2024-10-12 11:56:26 +0000

    Added QR Code scanner to homepgae

--- pages/Home/jsobjects/JSObject1/JSObject1.js
+++ pages/Home/jsobjects/JSObject1/JSObject1.js
@@ -1,5 +1,15 @@
 export default {
-	init(){
-		//ComponentsTable.pa
+	parseQRCode(){
+		let val = CodeScanner1.value;
+		try{
+			let jsonData = JSON.parse(val);
+			switch(jsonData.type){
+				case 'box':
+					navigateTo('Box',{id:jsonData.id })
+					break;
+			}
+		}catch(e){
+			showAlert(e,'error')
+		}
 	}
 }
